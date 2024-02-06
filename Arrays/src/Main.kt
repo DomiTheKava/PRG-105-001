06855557280d2b310d2017242220fe3e693a42d8
DomiTheKava <78154813+DomiTheKava@users.noreply.github.com>
2024-02-06 15:29:35 -0600

    Add files via upload

---
+++ Arrays/src/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val hotDawgStand = arrayOf("Hotdog", "Ketchup", "Pickes", "Onion", "Buns")
+
+    for (item in hotDawgStand) {
+        println(item)
+    }
+
+    println("... No mustard.")
+
+}
